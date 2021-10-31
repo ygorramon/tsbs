c850015c9ad85195be154b6ebc755a5d5a366595
ygorramon <ygor.ramon@ifce.edu.br>
2021-10-31 09:27:54 -0300

    Commit

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::middleware('auth')
 
 //Route::get('/links', 'SiteController@postsLinks' );
 
-Route::domain('{account}.tsbs.com.br')->group(function () {
+Route::domain('josy.tsbs.com.br')->group(function () {
     Route::get('/', function () {
         return 'Hello World';
     });
